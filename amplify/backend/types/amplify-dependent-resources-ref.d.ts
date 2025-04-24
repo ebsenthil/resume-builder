3408d1fd09172d2aa1fcd629cb92b41d98396879
ebsenthil <shri.senthilnathan@gmail.com>
2025-04-24 05:57:47 +0000

    Added Amplify backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "generateresume": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "resumebuilder7f9a0bfe": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
